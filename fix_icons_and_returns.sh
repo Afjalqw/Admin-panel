149ca1e38cd10e0083a3b376560c29d75656da85
Afjalqw <163416716+Afjalqw@users.noreply.github.com>
2026-07-19 15:18:54 +0530

    Delete fix_icons_and_returns.sh

--- fix_icons_and_returns.sh
+++
@@ -1,5 +0,0 @@
-sed -i 's/Icons.AutoMirrored.Filled.MergeType/Icons.Default.MergeType/g' app/src/main/java/com/example/ui/HomeDashboardScreen.kt
-sed -i 's/Icons.AutoMirrored.Filled.CallSplit/Icons.Default.CallSplit/g' app/src/main/java/com/example/ui/HomeDashboardScreen.kt
-sed -i 's/Icons.AutoMirrored.Filled.TextSnippet/Icons.Default.TextSnippet/g' app/src/main/java/com/example/ui/HomeDashboardScreen.kt
-
-sed -i '/return/d' app/src/main/java/com/example/MainActivity.kt
